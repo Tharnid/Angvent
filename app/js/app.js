80b882b3479c28fcb4f2158a07a68663147615dc
Tharnid <jeffmsmithdev@gmail.com>
2013-12-02 22:49:05 -0500

    created index, used ng-view, templates, and first route

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,30 @@
 'use strict';
 
-var angvent = angular.module('Angvent', ['ngResource', 'ngCookies'])
-    .factory('myCache', function($cacheFactory) {
-        return $cacheFactory('myCache', {capacity:3});
-    })
+var angvent = angular.module('Angvent', ['ngResource'])
+    .config(function ($routeProvider) { // , $locationProvider
+        $routeProvider.when('/newEvent',
+            {
+                templateUrl:'templates/NewEvent.html',
+                controller: 'EditEventController'
+            });
+        // $routeProvider.when('/events',
+        //     {
+        //         templateUrl: 'templates/EventList.html',
+        //         controller: 'EventListController'
+        //     });
+        // $routeProvider.when('/event/:eventId',
+        //     {
+        //         templateUrl: '/templates/EventDetails.html',
+        //         controller: 'EventController',
+        //         resolve: {
+        //             event: function($q, $route, eventData) {
+        //                 var deferred = $q.defer();
+        //                 eventData.getEvent($route.current.pathParams.eventId)
+        //                     .then(function(event) { deferred.resolve(event); });
+        //                 return deferred.promise;
+        //             }
+        //         }
+        //     });
+        // $routeProvider.otherwise({redirectTo: '/events'});
+        // $locationProvider.html5Mode(true);
+    });
